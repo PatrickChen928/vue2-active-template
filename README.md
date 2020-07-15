37f7a494f0a0497dcccb1034c21c902ab0415ebc
chenpeng <chenpeng01@weidian.com>
2020-07-15 19:30:31 +0800

    模型

--- README.md
+++ README.md
@@ -1,19 +1,3 @@
 # vue2-active-template
 
-## Project setup
-```
-yarn install
-```
-
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
-
-### Compiles and minifies for production
-```
-yarn build
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+一个vue2的在线编码器
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,91 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="left_edit--box">
+        <button @click="run">运行代码</button>
+        <textarea v-model="tempValue"></textarea>
+    </div>
+    <div class="J__right_show--box" id="showBox">
+
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Vue from 'vue';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      tempValue: '<template>\n\n\n</template>\n'
+    }
+  },
+  methods: {
+    getSource(source, type) {
+      const regex = new RegExp(`<${type}[^>]*`);
+      let val = source.match(regex);
+      if (!val) return '';
+      val = val[0];
+      return source.slice(
+        source.indexOf(val) + val.length,
+        source.lastIndexOf(val)
+      )
+    },
+    run() {
+      const template = this.getSource(this.tempValue, 'template');
+      const script = this.getSource(this.tempValue, 'script');
+      const style = this.getSource(this.tempValue, 'style');
+
+      // const result = Vue.extend({
+      //   template,
+      //   script
+      // });
+      // new result().$mount('#showBox');
+    }
   }
 }
 </script>
+
+<style lang="less" scoped>
+  .home {
+    display: flex;
+    box-sizing: border-box;
+    padding: 20px;
+    height: 100vh;
+    background-color: antiquewhite;
+    .left_edit--box {
+      flex: 1;
+      margin-right: 20px;
+      box-sizing: border-box;
+      button {
+        display: block;
+        width: 200px;
+        height: 36px;
+        line-height: 36px;
+        border: none;
+        border-radius: 4px;
+        color: #fff;
+        background-color: chocolate;
+      }
+      textarea {
+        margin-top: 20px;
+        padding: 10px;
+        box-sizing: border-box;
+        width: 100%;
+        height: 800px;
+        font-size: 16px;
+        border-radius: 4px;
+        color: #fff;
+        resize: none;
+        background-color:  rgb(97, 99, 94);
+      }
+    }
+    .J__right_show--box {
+      flex: 1;
+      margin-top: 56px;
+      height: 800px;
+      border-radius: 4px;
+      background-color: #fff;
+    }
+  }
+</style>
